13d16cedbd5de9732d7a50c8d5323d57e2377353
Gonz Saavedra <gonzalosaavedra@gmail.com>
2016-03-25 15:56:15 -0300

    Improved Redis URL setting for local/heroku setup

--- rekarma/__init__.py
+++ rekarma/__init__.py
@@ -3,8 +3,11 @@ import os
 from flask import Flask
 import redis
 
+# Redis URL setting in env REDISTOGO_URL or REDISTOGO_URL or default to localhost
+REDIS_URL = os.environ.get('REDISTOGO_URL',
+                           os.environ.get('REDIS_URL', 'localhost:6379'))
+
 app = Flask(__name__)
-redis_server = redis.from_url(os.environ.get('REDISTOGO_URL',
-                                             os.environ['REDIS_URL']))
+redis_server = redis.from_url(REDIS_URL)
 
 import rekarma.views
--- worker.py
+++ worker.py
@@ -5,8 +5,11 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_server = redis.from_url(os.environ.get('REDISTOGO_URL',
-                                             os.environ['REDIS_URL']))
+# Redis URL setting in env REDISTOGO_URL or REDISTOGO_URL or default to localhost
+REDIS_URL = os.environ.get('REDISTOGO_URL',
+                           os.environ.get('REDIS_URL', 'localhost:6379'))
+
+redis_server = redis.from_url(REDIS_URL)
 
 if __name__ == '__main__':
     with Connection(redis_server):
